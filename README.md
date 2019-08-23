19c1a31414f6843fbb1ef6b4462f4d0f901b4a5b
wfajkowski <wfajkowski@stxnext.pl>
2019-08-23 21:57:40 +0200

    create project environment

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # apka-pogodowa
 Weather app - the CodersCamp course project.
+
+Installation:
+Just run "npm install" command in the root project folder.
+
+Usage:
+1. To run Webpack with Babel run "npm run build" command.
+2. To run dev server run "npm run dev" command. Then You can run app under http://localhost:8080
+
+Remember that development mode compiles changes dynamically on the local machine and changes are not saved to dist folder automatically.
+To apply changes to the dist folder, run the command "npm run build".
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+          })
+    ]
+}

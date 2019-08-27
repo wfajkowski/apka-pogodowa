cf9f96281d8705628cb4b5b5cd6b97d0b908df89
Rafał Kamka <rafalkamka@gmail.com>
2019-08-27 12:55:34 +0200

    Simple data display

--- src/js/APIrequest.js
+++ src/js/APIrequest.js
@@ -3,34 +3,54 @@ export default class APIrequest {
     constructor(city, typeOfRequest){
         this.url = 'http://api.openweathermap.org/data/2.5/';
         this.appId = '15863c9657f4883dbb63c41d778aa851';
-        this.city = city;
+        this.city = city || defaultCity;
         this.typeOfRequest = typeOfRequest;
+        this.units = 'metric';
     }
     makeRequest(){
-        const result = fetch(`${this.url}${this.typeOfRequest}?q=${this.city}&APPID=${this.appId}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                
-                const wheaterDiv = document.querySelector('.wheater');
-                const icon = document.querySelector('.icon img');
-                const wheaterInfo = document.querySelector('.wheater-info');
-                const temp = document.querySelector('.temp');
-                const {main, sys, weather, wind } = data;
-                const mainElements = [main, sys, weather, wind];
-                
-                icon.src = `http://openweathermap.org/img/w/${weather[0].icon}.png`;
-                console.log(icon.src);
-                return mainElements;
-            })
-            .catch(error => console.log(error));
+        const result = fetch(
+          `${this.url}${this.typeOfRequest}?q=${this.city}&APPID=${this.appId}&units=${this.units}`
+        )
+          .then(res => res.json())
+          .then(data => {
+            // console.log(data);
+
+            const { main, sys, weather, wind } = data;
+            const mainElements = [main, sys, weather, wind];
+
+            return mainElements;
+          })
+          .catch(error => console.log(error));
             return result;
     }
     displayData(){
         // Test
-        console.log(this.makeRequest);
-        const temp = document.querySelector('.temp');
+       
+        const wheaterDiv = document.querySelector(".wheater");
+        const icon = document.querySelector(".icon img");
+        const wheaterInfo = document.querySelector(".wheater-info");
+        const temp = document.querySelector(".temp");
 
+         this.makeRequest()
+           .then(res => {
+             let result = [];
+             const data = res;
+            //  console.log("All info", data);
+             data.forEach(item => result.push(item));
+             return result;
+           })
+           .then((res) => {
+                console.log(JSON.stringify(res));
+                temp.innerHTML = JSON.stringify(res[0].temp) + "°C";
+                icon.src = `http://openweathermap.org/img/w/${res[2][0].icon}.png`;
+                wheaterInfo.innerHTML = `
+                    <p>Ciśnienie: <span>${JSON.stringify(res[0].pressure)}hPa</span></p>
+                    <p>Wilgotność: <span>${JSON.stringify(res[0].humidity)}%</span></p>
+                    <p>Temp min: <span>${JSON.stringify(res[0].temp_min)}°C</span></p>
+                    <p>Temp max: <span>${JSON.stringify(res[0].temp_max)}°C</span></p>
+                    <p>Siła wiatru: <span>${JSON.stringify(res[3].speed)}m/s</span></p>
+                `;
+            });
     }
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../css/style.css';
 import APIrequest from './APIrequest';
 
+const defaultCity = 'wroclaw';
 
 // Create div for test results
 const formDiv = document.createElement('div');
@@ -12,17 +13,21 @@ formDiv.appendChild(form);
 const inputTxt = document.createElement('input');
 inputTxt.setAttribute('type', 'text');
 form.appendChild(inputTxt);
-const submitBtn = document.createElement('input')
+const submitBtn = document.createElement('input');
 submitBtn.setAttribute('type', 'submit');
 form.appendChild(submitBtn);
+
+const defaultLocation = new APIrequest(defaultCity, "weather");
+defaultLocation.displayData();
 
 form.addEventListener('submit', event => {
     event.preventDefault();
     const cityName = inputTxt.value;
     const weather = new APIrequest(cityName, 'weather');
-    const data = weather.makeRequest();
-    console.log("data", data);
-    const result = data.then(console.log('jest'));
-    console.log("result", result[0]);
+    // const data = weather.makeRequest().then(weather.displayData());
+    // const data = weather.displayData();
+    // console.log("data", data);
+    // const result = data.then(console.log('jest'));
+    // console.log("result", result[0]);
     weather.displayData();
-})
+});

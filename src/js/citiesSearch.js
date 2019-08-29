58918a812df308d5519bf6be3375b06d89f64fff
wfajkowski <wfajkowski@stxnext.pl>
2019-08-29 22:18:01 +0200

    list style and return coords

--- src/js/citiesSearch.js
+++ src/js/citiesSearch.js
@@ -5,25 +5,30 @@ const input = document.querySelector('.input');
 const selectedPlace = document.querySelector('.sugestedList');
 const searchButton = document.getElementById('search');
 var val;
+var latitude;
+var longitude;
 
 function display() {
     finder.getCities(this.value);
 }
 
-function getCityId() {
+function setCityId() {
     finder.getCityId(selectedPlace);
 }
 
-export function returnCityId(id) {
-    console.log(id);
+export function returnCityId(id, lat, lon) {
+    console.log(id, lat, lon);
     val = id;
+    latitude = lat;
+    longitude = lon;
 }
 
 function passCityId() {
-    console.log(val);
+    console.log(val, latitude, longitude);
 }
 
 input.addEventListener('keyup', display);
-input.addEventListener('change', display);
-selectedPlace.addEventListener('click', getCityId);
+selectedPlace.addEventListener('click', setCityId, {
+    capture: true
+});
 searchButton.addEventListener('click', passCityId);
